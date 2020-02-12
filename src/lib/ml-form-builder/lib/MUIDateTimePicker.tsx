eb4c911ab416bdcf3181edb3714123e8954b9585
Saurabh Gupta <saurabhgupta@Saurabhs-MacBook-Pro-2.local>
2020-02-12 12:41:23 +0530

    fix onChange

--- src/lib/ml-form-builder/lib/MUIDateTimePicker.tsx
+++ src/lib/ml-form-builder/lib/MUIDateTimePicker.tsx
@@ -18,13 +18,13 @@ export const MUIDatePicker: FC<IFieldProps & { fieldProps?: IMUIDatePickerProps 
         helperText: (fieldError || ''),
         onChange: (date: any) => {
             if (!date) {
-                formikProps.setFieldValue(fieldProps.name, '', false);
+                formikProps.setFieldValue(fieldProps.name, date, false);
                 return;
             }
             const dateValue = (outputFormat === 'date') ? date : date.format(outputFormat || fieldProps.format || 'YYYY-MM-DD');
             formikProps.setFieldValue(fieldProps.name, dateValue, false);
         },
-        inputValue: get(formikProps, `values.${fieldProps.name}`) || '',
+        inputValue: get(formikProps, `values.${fieldProps.name}`),
         onError: (error: string) => {
             // handle as a side effect
             if (error !== fieldError) {
